b80a4fe314811cff289de8cd30583eabfc709bc0
Lucy Wang <wxitb2017@gmail.com>
2021-06-23 18:54:05 +0800

    0.1.0

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 ## BB - Backup Batteries
 
 Some utils, that are useful sometimes.
+
+
+## `bb.clojure/cond*`
+
+This is an alias to `better-cond.core/cond` in the [better-cond](https://github.com/Engelberg/better-cond) library 
+
+Other funtions in better-cond family is also included:
+- `bb.clojure/when-let*`
+- `bb.clojure/when-some*`
+- `bb.clojure/if-some*`
+- `bb.clojure/if-let*`
+
+## `bb.path`
+
+Provides path manipulation functions like:
+- `bb.path/join`
+- `bb.path/basename`
+- `bb.path/dirname`
+- `bb.path/exists?`
--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject bb "0.1.0-SNAPSHOT"
+(defproject bb "0.1.0"
   :description "Some utils, that are useful sometimes."
   :url "https://github.com/lucywang000/bb"
   :min-lein-version "2.5.0"
 
   :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
-                 [vvvvalvalval/scope-capture "0.3.2"]
-                 [medley "1.3.0"]
-                 [cprop "0.1.17"
+                 #_[vvvvalvalval/scope-capture "0.3.3-s1"]
+                 #_[medley "1.3.0"]
+                 #_[cprop "0.1.18"
                   :exclusions [org.clojure/clojure]]]
 
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
